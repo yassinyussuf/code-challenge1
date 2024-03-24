9584a27ff99823ab0f65042edacfa7ed9245ac4e
yassinyussuf <yassinyussuf414@gmail.com>
2024-03-24 22:46:09 +0300

    complete assingment

---
+++ speed.js
@@ -0,0 +1,16 @@
+// prompt the user to enter his details
+let prompt = require("prompt-sync")({ sigint:true });
+
+let name = prompt("can you put your name: ");
+let speed = prompt("can you put your speed: ");
+
+//the line below is the code that will be executed if the user enters
+Improve 
+function speeding(speed){
+    if (speed <70){
+        return "ok";
+    } else {
+        let demeritpoints = math.trunc((speed - 70) / 5);
+    }
+}
+
